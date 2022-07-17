5113276c96f5221f1e53430fc05a6a270b27d17d
yapiko <bloomingjewels7021@gmail.com>
2022-07-17 14:08:49 +0900

    Javaform1

---
+++ src/main/java/com/example/demoform/DemoformApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoformApplication.class, args);
+	}
+
+}
